fedefa671283bdea9b48409e52f5f4275fc443c6
DH <dh@DHs-MacBook-Pro.local>
2019-02-28 15:09:10 -0500

    Testing deploy script with style changes

--- build/precache-manifest.ece8676ecc8e6f84159e86103f8fdfc8.js
+++ build/precache-manifest.ece8676ecc8e6f84159e86103f8fdfc8.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.9eb600ee.js"
   },
   {
-    "revision": "483d7c36bc66c9206092",
-    "url": "./static/js/main.483d7c36.chunk.js"
+    "revision": "bc034634d1ba05c79ba9",
+    "url": "./static/js/main.bc034634.chunk.js"
   },
   {
     "revision": "015ea03cb7b11a05a1c5",
     "url": "./static/js/2.015ea03c.chunk.js"
   },
   {
-    "revision": "483d7c36bc66c9206092",
-    "url": "./static/css/main.60ffa150.chunk.css"
+    "revision": "bc034634d1ba05c79ba9",
+    "url": "./static/css/main.77977e77.chunk.css"
   },
   {
-    "revision": "afa79bad7e224c2343bb4e546159f0b9",
+    "revision": "16bb85a5ca5cad094d3c088b96d54cab",
     "url": "./index.html"
   }
 ];
