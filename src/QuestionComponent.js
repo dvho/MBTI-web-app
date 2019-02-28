8dde262e85fb2ad8f7cb8794f88d3662351c24c6
DH <dh@DHs-MacBook-Pro.local>
2019-02-28 14:33:40 -0500

    Restyle graphs

--- src/QuestionComponent.js
+++ src/QuestionComponent.js
@@ -12,8 +12,8 @@ function QuestionComponent (props) {
                 <div className="box-2"><input type="range" min="0" max="100" className="slider" id={props.item.id} onChange={props.sliding}/></div>
                 <div className="box-3">{props.item.answerB}</div>
             </div>
-            <div style={{height: `${(props.item.choice * -1.5) + 75}px`, marginTop: `-${(props.item.choice * -1.5) + 75}px`, opacity: `${((((props.item.choice) * -1)+50)/200)}`}} className="graphs left-graph"></div>
-            <div style={{height: `${(props.item.choice * 1.5) + 75}px`, marginTop: `-${(props.item.choice * 1.5) + 75}px`, opacity: `${(props.item.choice + 50)/200}`}} className="graphs right-graph"></div>
+            <div style={{height: `${(props.item.choice * -1.5) + 75}px`, marginTop: `-${(props.item.choice * -1.5) + 75}px`, opacity: `${((((props.item.choice) * -1)+90)/200)}`}} className="graphs left-graph"></div>
+            <div style={{height: `${(props.item.choice * 1.5) + 75}px`, marginTop: `-${(props.item.choice * 1.5) + 75}px`, opacity: `${(props.item.choice + 90)/200}`}} className="graphs right-graph"></div>
         </div>
     )
 }
