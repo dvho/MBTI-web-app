f907fe15aa5c03bd3c0a6f50681ab1add23546fa
DH <dh@DHs-MacBook-Pro.local>
2019-03-05 10:58:32 -0500

    Calculate all 8 preferences and update in state

--- precache-manifest.0fe112866c98e09ca3109878b45ea0e7.js
+++ precache-manifest.0fe112866c98e09ca3109878b45ea0e7.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.9eb600ee.js"
   },
   {
-    "revision": "8ee497975f9db0a2f306",
-    "url": "./static/js/main.8ee49797.chunk.js"
+    "revision": "d96c86306ccd45ff3f53",
+    "url": "./static/js/main.d96c8630.chunk.js"
   },
   {
     "revision": "015ea03cb7b11a05a1c5",
     "url": "./static/js/2.015ea03c.chunk.js"
   },
   {
-    "revision": "8ee497975f9db0a2f306",
-    "url": "./static/css/main.c4c821e7.chunk.css"
+    "revision": "d96c86306ccd45ff3f53",
+    "url": "./static/css/main.01cf76b6.chunk.css"
   },
   {
-    "revision": "f935dc222f85bb94623bc6809f0e1ca6",
+    "revision": "cf90359468e9d4e7100c991b7e80006f",
     "url": "./index.html"
   }
 ];
