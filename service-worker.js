e0dfd41d7efd1ac22c80490e4deb40d351d05969
DH <dh@DHs-MacBook-Pro.local>
2019-03-08 12:27:16 -0500

    Animate and style articles

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ac2d81ec42bad8c6f307720846a6df59.js"
+  "./precache-manifest.7d6ee757a748268fd685d64ca89a6a8f.js"
 );
 
 workbox.clientsClaim();
