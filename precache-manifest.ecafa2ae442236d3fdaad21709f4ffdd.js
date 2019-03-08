052b1e1d958cb3b6913a94a61cefb77d3ae4ed20
DH <dh@DHs-MacBook-Pro.local>
2019-03-08 13:07:38 -0500

    Style articles

--- precache-manifest.ecafa2ae442236d3fdaad21709f4ffdd.js
+++ precache-manifest.ecafa2ae442236d3fdaad21709f4ffdd.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.9eb600ee.js"
   },
   {
-    "revision": "d130d309bd1f42e55953",
-    "url": "./static/js/main.d130d309.chunk.js"
+    "revision": "383772ca6ce139ddc8b2",
+    "url": "./static/js/main.383772ca.chunk.js"
   },
   {
     "revision": "015ea03cb7b11a05a1c5",
     "url": "./static/js/2.015ea03c.chunk.js"
   },
   {
-    "revision": "d130d309bd1f42e55953",
-    "url": "./static/css/main.d72c02a1.chunk.css"
+    "revision": "383772ca6ce139ddc8b2",
+    "url": "./static/css/main.ced6f83f.chunk.css"
   },
   {
-    "revision": "aaa3ac112577e3d47a499020e4c85f1b",
+    "revision": "f519fc84523be1c7adc490f74a595052",
     "url": "./index.html"
   }
 ];
