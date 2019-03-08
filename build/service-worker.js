9fc761151d2ce2d90d6ea3642330631c56a8c35e
DH <dh@DHs-MacBook-Pro.local>
2019-03-08 13:07:38 -0500

    Style articles

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7d6ee757a748268fd685d64ca89a6a8f.js"
+  "./precache-manifest.ecafa2ae442236d3fdaad21709f4ffdd.js"
 );
 
 workbox.clientsClaim();
--- src/components/IntroComponent.js
+++ src/components/IntroComponent.js
@@ -5,11 +5,11 @@ function IntroComponent () {
         <div>
             <h1 className='app-title'>Continuous-Choice Myers-Briggs Type Indicator (MBTI)</h1>
             <p className='app-intro'>This exam is an upgrade to the iconic forced-choice Myers-Briggs Type Indicator used ubiquitously throughout the world. The original forces the examinee to choose starkly between two extremes, many of which are not opposite, having caused a long history of unease and inaccurate data. This updated version allows the examinee to indicate preferences more granularly by leaning toward one answer or the other at varying degrees rather than choosing entirely between the two.</p>
-            <ol className='app-directions'>
-                <li>Scroll through the questions and slide sliders left and right according to your preferences.</li>
-                <li>Do not over-analyze the questions. Answer quickly and honestly, and keep scrolling.</li>
-                <li>Answer questions the <i>way you are</i> not the <i>way you’d like to be seen by others</i>.</li>
-            </ol>
+            <div className='app-directions'>
+                <p>1. Scroll through the questions and slide sliders left and right according to your preferences.</p>
+                <p>2. Do not over-analyze the questions. Answer quickly and honestly, and keep scrolling.</p>
+                <p>3. Answer questions the <i>way you are</i> not the <i>way you’d like to be seen by others</i>.</p>
+            </div>
 
         </div>
     )
