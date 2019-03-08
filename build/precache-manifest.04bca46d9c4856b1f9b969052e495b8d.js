041a0df3f8e9be5ceee1a23ae05f47da9be9056b
DH <dh@DHs-MacBook-Pro.local>
2019-03-07 20:18:51 -0500

    Create overlapping flex preferenes graph headings

--- build/precache-manifest.04bca46d9c4856b1f9b969052e495b8d.js
+++ build/precache-manifest.04bca46d9c4856b1f9b969052e495b8d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.9eb600ee.js"
   },
   {
-    "revision": "217842f144b057c1de1b",
-    "url": "./static/js/main.217842f1.chunk.js"
+    "revision": "06c308d4652cf07bf5f6",
+    "url": "./static/js/main.06c308d4.chunk.js"
   },
   {
     "revision": "202261632717f9e94b49",
     "url": "./static/js/2.20226163.chunk.js"
   },
   {
-    "revision": "217842f144b057c1de1b",
-    "url": "./static/css/main.8fb57d6e.chunk.css"
+    "revision": "06c308d4652cf07bf5f6",
+    "url": "./static/css/main.fc8812aa.chunk.css"
   },
   {
-    "revision": "7159ad7131c2a09c8b9f88998144624d",
+    "revision": "7df7fda45ec2c111aabf2122417dcafe",
     "url": "./index.html"
   }
 ];
--- src/components/PreferencesGraph.js
+++ src/components/PreferencesGraph.js
@@ -2,30 +2,38 @@ import React from 'react';
 
 function PreferencesGraph(props) {
     return(
-        <div className='preferences-graph-container'>
-            <div className='preferences-graph-container-box'><div className='label'>Extroversion</div>
-                <div style={{height: props.state.extroversion * 3}} className='preference graph-bar-left extroversion'>{props.state.extroversion}</div>
+        <div>
+            <div className='preferences-pairs'>
+                <div className='graph-heading'>E/I</div>
+                <div className='graph-heading'>S/N</div>
+                <div className='graph-heading'>T/F</div>
+                <div className='graph-heading'>J/P</div>
             </div>
-            <div className='preferences-graph-container-box'><div className='label'>Introversion</div>
-                <div style={{height: props.state.introversion * 3}} className='preference graph-bar-right introversion'>{props.state.introversion}</div>
-            </div>
-            <div className='preferences-graph-container-box'><div className='label'>Sensing</div>
-                <div style={{height: props.state.sensing * 3}} className='preference graph-bar-left sensing'>{props.state.sensing}</div>
-            </div>
-            <div className='preferences-graph-container-box'><div className='label'>Intuition</div>
-                <div style={{height: props.state.intuition * 3}} className='preference graph-bar-right intuition'>{props.state.intuition}</div>
-            </div>
-            <div className='preferences-graph-container-box'><div className='label'>Thinking</div>
-                <div style={{height: props.state.thinking * 3}} className='preference graph-bar-left thinking'>{props.state.thinking}</div>
-            </div>
-            <div className='preferences-graph-container-box'><div className='label'>Feeling</div>
-                <div style={{height: props.state.feeling * 3}} className='preference graph-bar-right feeling'>{props.state.feeling}</div>
-            </div>
-            <div className='preferences-graph-container-box'><div className='label'>Judging</div>
-                <div style={{height: props.state.judging * 3}} className='preference graph-bar-left judging'>{props.state.judging}</div>
-            </div>
-            <div className='preferences-graph-container-box'><div className='label'>Perceiving</div>
-                <div style={{height: props.state.perceiving * 3}} className='preference graph-bar-right perceiving'>{props.state.perceiving}</div>
+            <div className='preferences-graph-container'>
+                <div className='preferences-graph-container-box'><div className='label'>Extroversion</div>
+                    <div style={{height: props.state.extroversion * 3}} className='preference graph-bar-left'>{props.state.extroversion}</div>
+                </div>
+                <div className='preferences-graph-container-box'><div className='label'>Introversion</div>
+                    <div style={{height: props.state.introversion * 3}} className='preference graph-bar-right'>{props.state.introversion}</div>
+                </div>
+                <div className='preferences-graph-container-box'><div className='label'>Sensing</div>
+                    <div style={{height: props.state.sensing * 3}} className='preference graph-bar-left'>{props.state.sensing}</div>
+                </div>
+                <div className='preferences-graph-container-box'><div className='label'>Intuition</div>
+                    <div style={{height: props.state.intuition * 3}} className='preference graph-bar-right'>{props.state.intuition}</div>
+                </div>
+                <div className='preferences-graph-container-box'><div className='label'>Thinking</div>
+                    <div style={{height: props.state.thinking * 3}} className='preference graph-bar-left'>{props.state.thinking}</div>
+                </div>
+                <div className='preferences-graph-container-box'><div className='label'>Feeling</div>
+                    <div style={{height: props.state.feeling * 3}} className='preference graph-bar-right'>{props.state.feeling}</div>
+                </div>
+                <div className='preferences-graph-container-box'><div className='label'>Judging</div>
+                    <div style={{height: props.state.judging * 3}} className='preference graph-bar-left'>{props.state.judging}</div>
+                </div>
+                <div className='preferences-graph-container-box'><div className='label'>Perceiving</div>
+                    <div style={{height: props.state.perceiving * 3}} className='preference graph-bar-right'>{props.state.perceiving}</div>
+                </div>
             </div>
         </div>
     )
